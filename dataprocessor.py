337c61e793c3c101b157d4cb6cc81e4f38f12571
ModelChief30 <88141819+artificialcommander03@users.noreply.github.com>
2021-08-10 21:15:49 +0100

    Update dataprocessor.py
    Fixed the loop in duplicate batch so the batch_ids aren't compared with themselves and made unformatted print strings formatted. Also put the file reader code into a main function with a filename argument which returns either of the strings SUCCESS or FAILURE.

--- dataprocessor.py
+++ dataprocessor.py
@@ -11,36 +11,44 @@ def min_check(reading):
     return reading < 0.0
 
 def duplicate_batch(batch_ids):
-    for batch_id in batch_ids:
-        for batch_id2 in batch_ids:
-            if batch_id == batch_id2:
+    for i in range(len(batch_ids)):
+        for j in range(i+1, len(batch_ids)):
+            if batch_ids[i] == batch_ids[j]:
                 return True
     return False
 
-file_name = input('Please enter your filename: ')
-with open(file_name, 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    batch_ids = []
-    for row in csv_reader:
-        for field in row:
-            if (presence_check(row[field])):
-                print(f"File invalid. The data for batch_id {row['batch_id']} is missing at least one reading")
-                sys.exit()
-        readings = [float(row['reading1']), float(row['reading2']), float(row['reading3']), float(row['reading4']),
-                    float(row['reading5']), float(row['reading6']), float(row['reading7']), float(row['reading8']),
-                    float(row['reading9']), float(row['reading10'])]
-        for reading in readings:
-            print(reading)
-            if max_check(reading):
-                print("File invalid. Reading value {reading} for batch id {row['batch_id']} exceeds max value 9.9")
-                sys.exit()
-            elif min_check(reading):
-                print("File invalid. Reading value {reading} for batch id {row['batch_id']} below min value 0.0")
-                sys.exit()
-        batch_ids.append(int(row['batch_id']))
-    if duplicate_batch(batch_ids):
-        print("File invalid. There are duplicate batch ids in the file")
-        sys.exit()
-    print("File valid. Storing...")
 
+def processor(file_name):
+    with open(file_name, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        batch_ids = []
+        for row in csv_reader:
+            for field in row:
+                if (presence_check(row[field])):
+                    print(f"File invalid. The data for batch_id {row['batch_id']} is missing at least one reading")
+                    return "FAILURE"
+            readings = [float(row['reading1']), float(row['reading2']), float(row['reading3']), float(row['reading4']),
+                        float(row['reading5']), float(row['reading6']), float(row['reading7']), float(row['reading8']),
+                        float(row['reading9']), float(row['reading10'])]
+            for reading in readings:
+                if max_check(reading):
+                    print(f"File invalid. Reading value {reading} for batch id {row['batch_id']} exceeds max value 9.9")
+                    return "FAILURE"
+                elif min_check(reading):
+                    print(f"File invalid. Reading value {reading} for batch id {row['batch_id']} below min value 0.0")
+                    return "FAILURE"
+            batch_ids.append(int(row['batch_id']))
+        print(batch_ids)
+        if duplicate_batch(batch_ids):
+            print("File invalid. There are duplicate batch ids in the file")
+            return "FAILURE"
+        
+        return "SUCCESS"
+
+def main():
+    file_name = input("Please enter a filename: ")
+    print(processor(file_name))
+
+if __name__ == "__main__":
+    main()
 
